cd534a3d16ab76607a3e00d7f137109d02c90389
John Welch <jwelch0015@gmail.com>
2016-11-17 21:22:51 -0500

    Integrating John-contact.php into site.

--- page-contact.php
+++ page-contact.php
@@ -87,23 +87,7 @@
 
 				<aside>
 					<h3>What's Your Cheese?</h3>
-					<form>
-						<label>First Name</label>
-						<input type="text" name="First Name" placeholder="First Name" required="required">
-						<label>Last Name</label>
-						<input type="text" name="Last Name" placeholder="Last Name" required="required">
-						<label>Email Address</label>
-						<input type="email" name="Email Address" placeholder="Email Address" required="required">
-						<label>Tell us about yourself?</label>
-						<textarea name="Your Message Here" placeholder="What's going on?"></textarea>
-						<label>Subscribe to our Newsletter!</label>
-						<radio></radio>
-
-						<button name="submit" type="submit" value="submit">
-							<img src="<?php echo get_template_directory_uri();?>/library/images/sky-button.svg" alt="Let's Get Started!">
-						</button>
-
-					</form>
+					<?php include_once("library/partials/john-contact.php") ?>
 				</aside>
 			</section>
 		
