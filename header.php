7e85d3f1e3a65a3e918c640b7b2af573ab304409
Patrick Hanus <patrick@skymse.com>
2016-11-20 16:15:01 -0500

    Tweaked the Error 404 Page

--- header.php
+++ header.php
@@ -112,6 +112,34 @@
 						   <h1>Captain, We May Have a problem</h1>
 							<h4>Whoops! This page is missing!</h4>
 
+							<p>You 
+								<?php
+								$adminemail = get_option('admin_email'); #the administrator email address, according to wordpress
+								$website = get_bloginfo('url'); #gets your blog's url from wordpress
+								$websitename = get_bloginfo('name'); #sets the blog's name, according to wordpress
+
+								  if (!isset($_SERVER['HTTP_REFERER'])) {
+								    #politely blames the user for all the problems they caused
+								        echo "tried going to "; #starts assembling an output paragraph
+									$casemessage = "All is not lost!";
+								  } elseif (isset($_SERVER['HTTP_REFERER'])) {
+								    #this will help the user find what they want, and email me of a bad link
+									echo "clicked a link to"; #now the message says You clicked a link to...
+								        #setup a message to be sent to me
+									$failuremess = "A user tried to go to $website"
+								        .$_SERVER['REQUEST_URI']." and received a 404 (page not found) error. ";
+									$failuremess .= "It wasn't their fault, so try fixing it.  
+								        They came from ".$_SERVER['HTTP_REFERER'];
+									mail($adminemail, "Bad Link To ".$_SERVER['REQUEST_URI'],
+								        $failuremess, "From: $websitename <noreply@$website>"); #email you about problem
+									$casemessage = "An administrator has been emailed 
+								        about this problem, too.";#set a friendly message
+								  }
+								  echo " ".$website.$_SERVER['REQUEST_URI']; ?> 
+								and it doesn't exist. <?php echo $casemessage; ?>  You can click back 
+								and try again or try going back to our <a href="<?php echo get_home_url(); ?>">home page</a>!
+								</p>
+
 						<?php } ?><!--End Error 404-->
 
 
