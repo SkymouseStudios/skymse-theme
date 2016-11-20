ef08c71b66e9dd05f686d28e4ed37f1d44dd26d5
John Welch <john@skymse.com>
2016-11-20 17:23:19 -0500

    Contact Form PHP Cleanup

--- library/partials/john-contact.php
+++ library/partials/john-contact.php
@@ -6,7 +6,6 @@ John's Contact Form
 
 <h3>What's Your Cheese?</h3>
 
-<form name="contactform" method="post" action="send_form_email.php">
 	<label for="first_name">First Name</label>
 	<input type="text" name="first_name" placeholder="First Name" required="required">
 	<label for="last_name">Last Name</label>
@@ -18,10 +17,6 @@ John's Contact Form
 	<label>Subscribe to our Newsletter!</label>
 	<radio></radio>
 
-    <input type="hidden" name="retURL" value="<?php echo get_home_url();?>">
-    
-	<input name="submit" type="submit" value="submit"
-		img src="<?php echo get_template_directory_uri();?>/library/images/sky-button.svg" alt="Let's Get Started!">
 
 </form>
 
